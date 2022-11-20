3563e7dc045d7a3e312cada1d3d8ea5026b137a1
JJPaccito <j.ovchinikov@gmail.com>
2022-11-20 09:08:53 +0200

    second commint

--- test012 age_counting.py
+++ test012 age_counting.py
@@ -4,6 +4,6 @@ name = input("Hi, what is your name? ")
 birth_year = input("input year of your birth: ")
 today = datetime.date.today()
 year = today.year
-myAge = year - int(birth_year)
+yourAge = year - int(birth_year)
 print(name + ", " + "your age is:")
-print(myAge)
+print(yourAge)
